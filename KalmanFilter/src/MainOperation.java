c202496afdf135d15acee92c96b3c67b52de942a
musagokhan <107429015+musagokhan@users.noreply.github.com>
2024-05-24 01:52:22 +0300

    v.1.0.1 24052024_0128: add second object

--- KalmanFilter/src/MainOperation.java
+++ KalmanFilter/src/MainOperation.java
@@ -15,13 +15,23 @@ public class MainOperation {
 		double accelerationY = 0.65;
 		double accelerationZ = 1;
 		
-		double[] newStateVector = {posX, posY, posZ, speedX, speedY, speedZ, accelerationX, accelerationY, accelerationZ};
+		double[] track0StateVector = {posX, posY, posZ, speedX, speedY, speedZ, accelerationX, accelerationY, accelerationZ};
+		KF track0 = new KF(track0StateVector);
+		System.out.println("init track0 : " + track0);
+
+		double[] track1StateVector = {posX+1, posY-1, posZ+10, speedX, speedY, speedZ, accelerationX, accelerationY, accelerationZ};
+		KF track1 = new KF(track1StateVector);
+		System.out.println("init track1 : " + track1);
+
+		for (double i=0 ; i<10 ; i++){
+			track0.getKFPredicted(i);
+			System.out.println("Step " + i + " - pred track0 : " + track0);
+			track1.getKFPredicted(i);
+		}
 		
-		KF track = new KF(newStateVector);
-		System.out.println("init track : " + track);
 		
-		track.getKFPredicted(3.0);
-		System.out.println("pred track : " + track);
+		
+		
 		
 		
 		System.out.println("--- Kalman Filter Stop ---");
